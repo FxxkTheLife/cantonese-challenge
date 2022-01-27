8db522d87f037021a836020fac55c43146966b95
Sidney Liu <cloxnu@outlook.com>
2022-01-27 10:38:07 +0800

    Fix:
    * cannot speak cantonese on safari

--- speak.js
+++ speak.js
@@ -10,32 +10,25 @@ function getVoices() {
     }, 100);
 }
 
-function cantoneseSpeechLangParams() {
-    const cantoneseFilterVoices = voices.filter(item => {
+function cantoneseSpeechVoice() {
+    const cantoneseVoice = voices.find(item => {
         return item.lang == lang;
     });
-    if (cantoneseFilterVoices.length >= 1) {
-        const cantoneseVoice = cantoneseFilterVoices[0];
-        return {
-            voiceURI: cantoneseVoice.voiceURI,
-            lang: cantoneseVoice.lang,
-            volumn: 1,
-            pitch: 1,
-            rate: 1,
-        };
-    }
-    return {};
+    return cantoneseVoice;
 }
 
 function speak(text) {
-    speechSynthesis.cancel();
+    window.speechSynthesis.cancel();
     let speech = new SpeechSynthesisUtterance(text);
-    const params = cantoneseSpeechLangParams();
-    if (params.lang !== lang) {
+    const voice = cantoneseSpeechVoice();
+    if (!voice) {
+        alert("Sorry~ 播放失败了");
+    }
+    if (voice.lang !== lang) {
         return;
     }
-    Object.assign(speech, params);
-    speechSynthesis.speak(speech);
+    speech.voice = voice;
+    window.speechSynthesis.speak(speech);
 }
 
 getVoices();
