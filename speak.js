e6a5e16282b27bad8df46ccb9b96cfd69d52e696
Sidney Liu <cloxnu@outlook.com>
2022-01-29 22:54:15 +0800

    Opt: (#8)
    * UI adjustment
    * More alert

--- speak.js
+++ speak.js
@@ -11,10 +11,20 @@ function getVoices() {
 }
 
 function cantoneseSpeechVoice() {
-    const cantoneseVoice = voices.find(item => {
+    const cantoneseVoices = voices.filter(item => {
         return item.lang == lang;
     });
-    return cantoneseVoice;
+    if (cantoneseVoices.length <= 0) {
+        return undefined;
+    }
+    const localCantoneseVoices = cantoneseVoices.filter(item => {
+        return item.localService == true;
+    });
+
+    if (localCantoneseVoices.length >= 0) {
+        return localCantoneseVoices[0];
+    }
+    return cantoneseVoices[0];
 }
 
 function speak(text) {
@@ -22,7 +32,12 @@ function speak(text) {
     let speech = new SpeechSynthesisUtterance(text);
     const voice = cantoneseSpeechVoice();
     if (!voice) {
-        alert("Sorry~ 播放失败了");
+        alert("Sorry~ 播放失败了，请稍后再试");
+        return;
+    }
+    if (!voice.localService) {
+        alert("当前语音非本地服务，可能需要等待较长时间");
+        return;
     }
     if (voice.lang !== lang) {
         return;
